b5723b71ce8b03acee9c1e9e451a43ced3768558
VahidM313 <vahidood@gmail.com>
2023-03-23 18:04:45 +0330

    make transition table

--- Phases/phase_1.js
+++ Phases/phase_1.js
@@ -1,33 +1,36 @@
 const data = require("./../samples/phase1-sample/in/input1.json");
+
+// parsing json 
+
 const states = data.states.replace(/'|{|}/g, "").split(",");
 const input_symbols = data.input_symbols.replace(/'|{|}/g, "").split(",");
 const initial_state = data.initial_state;
 const final_states = data.final_states.replace(/'|{|}/g, "").split(",");
-let table = [];
 let transitions = Object.entries(data.transitions);
-let column = [null,null,null,null];
+
+// convert transition object of json to transition table
+
+let transTable = [];
+let column = [null, "", "", ""];
+
 transitions = transitions.map((i) => {
   i[1] = Object.entries(i[1]);
   i[1] = i[1].map((j) => {
     j[1] = j[1].replace(/'|{|}/g, "").split(",");
     return j;
   });
-  // console.log(`${i[0]}:`);
   column[0] = i[0];
   i[1].forEach((element) => {
     if (element[0] === "") element[0] = "λ";
-    // console.log(element);
-    if (element[0] === "λ") column[3] = element[1];
-    else if (element[0] === "a") column[1] = element[1];
-    else if (element[0] === "b") column[2] = element[1];
-    // console.log(`\t${element[0]}:\n\t\t${element[1]}`);
+    if (element[0] === "λ") column[3] += element[1];
+    else if (element[0] === "a") column[1] += element[1];
+    else if (element[0] === "b") column[2] += element[1];
   });
-  table.push(column);
-  // console.log(column);
-  column = [null,null,null,null];
+  transTable.push(column);
+  column = [null, "", "", ""];
   i = i[1];
   return i;
 });
 
-console.table(table);
-// console.log(table);
+console.log(final_states);
+console.table(transTable);
